0e1ba8f151d306c5facfe49b95ab33aa250bb9b2
Ghosr9841 <asubedi778@gmail.com>
2024-11-07 18:02:51 +0545

    Transition Effect added to every page, Header and Footer remade and made changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center text-whitelogo'>
-        <Image src={assets.logo} alt='' width={120}/>
+       <div className="flex items-center mr-4">
+         <Image src={assets.logo} alt='chess logo' width={90}/>
+        <h1 className='text-white mx-4'>Chess Nepal</h1>
+        </div>
         <p className="text-sm">All right reserved. Copyright @ChessNepal</p>
         <div className="flex ">
         <Link
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,9 +12,14 @@ const NavBar = () => {
     <div className='py-5 px-5 md:px-12 lg:px-28'>
       <TransitionEffect/>
       <div className="flex justify-between items-center">
-        <Link href='/' className='hover:shadow-[-7px_-7px_0px_#363636]'>
-        <Image src={logo} width={40} alt ='logo' className='w-[80px]  sm:w-auto'/>
+       <div className="flex items-center">
+        <Link href='/' className=''>
+        <Image src={logo} width={40} alt ='logo' className='w-[80px]  sm:w-auto hover:border-2 hover:border-black'/>
         </Link>
+        <Link href='/' className=''>
+        <h1 className="font-bold mx-4 font-poppins">Chess Nepal</h1>
+        </Link>
+       </div>
         
         <button className="flex items-center gap-2 font-poppins font-medium py-1 
         px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_black]">
